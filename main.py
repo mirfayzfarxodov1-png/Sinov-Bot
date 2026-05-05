2b6d4550135033dc33d17ecc3287478bcb9faac7
mirfayzfarxodov1-png <mirfayzfarxodov1@gmail.com>
2026-05-05 07:21:00 +0500

    Refactor main function for synchronous execution
    Refactored main function to remove asyncio and simplify bot startup.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import logging
-import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-# BOT TOKEN
 TOKEN = "8601024606:AAHKA5r36MdnAu-83V9qUmlxsTwBD3xe84w"
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
@@ -19,18 +17,13 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif text == "narxa":
         await update.message.reply_text("25000 som")
 
-async def main():
+def main():
     application = Application.builder().token(TOKEN).build()
     
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling()
-    
-    # Botni ishga tushirib turish
-    await asyncio.Event().wait()
+    application.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
